016218e869112ab7871ee64aeb50d4ab302aa13d
jjp6315 <jjp6315@psu.edu>
2020-09-17 15:12:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Author: Ji Woong Park jjp6315@psu.edu
+# Collaborator: Joshua Watt jhw5304@psu.edu
+# Collaborator: Claudio Tapia-Manon cbt311@psu.edu
+# Collaborator: Dongsheng Zang dkz5086@psu.edu
+# Section: 1-12
+# Breakout: 9
+
+def sum_n(n):
+  if n == 0:
+    return n
+  else: 
+    return n + sum_n(n-1)
+    
+
+def print_n (s, n):
+  if n == 0: 
+    return
+  else:
+    print (f"{s}")
+    print_n(s, n-1)
+
+def run():
+  n = input("Enter an int: ")
+  n = int(n)
+  print(f"sum is {sum_n(n)}.")
+  s = input("Enter a string: ")
+
+  print_n(s, n)
+
+if __name__ == "__main__":
+  run()
